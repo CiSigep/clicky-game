00dc1cb09cf4b1e096ea0ba31839f62c52596115
CiSigEp <cisigep@gmail.com>
2019-09-10 12:40:08 -0500

    create json file to contain card array data, add shuffle function for cards

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,32 +1,54 @@
 import React from "react";
 import PointCounter from "./PointCounter";
 import Card from "./Card";
+import cards from "../cards.json";
 
 class Game extends React.Component {
   constructor(props){
     super(props);
 
     this.cardId = -1;
+
+    this.state = {
+      cards: cards
+    }
   }
 
 
   update = (cardId) => {
-    if(this.cardId === -1)
+    if(this.cardId === -1){
+      this.counter.incrementPoints();
       this.cardId = cardId;
+    }
     else if(this.cardId === cardId)
       this.counter.incrementPoints();
     else if(this.cardId !== cardId){
       this.cardId = -1;
       this.counter.resetPoints();
     }
+    this.shuffleCards();
+  }
+
+  shuffleCards = () => {
+    let cardsArray = this.state.cards.slice(0);
+
+    for (let i = cardsArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [cardsArray[i], cardsArray[j]] = [cardsArray[j], cardsArray[i]];
+    }
+
+    this.setState({cards: cardsArray});
   }
 
   render() {
     return(
     <div>
       <PointCounter ref={(counter) => {this.counter = counter}}/>
-      <Card gameUpdate={this.update} cardId={1} altTxt="fool" cardImg="img/fool.png"/>
-      <Card gameUpdate={this.update} cardId={2} altTxt="world" cardImg="img/world.png"/>
+      {
+        this.state.cards.map(ele => {
+          return (<Card key={ele.cardId} cardId={ele.cardId} cardImg={ele.cardImg} altTxt={ele.altTxt} gameUpdate={this.update}/>);
+        })
+      }
     </div>
     );
   }
