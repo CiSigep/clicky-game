bba9cf19a2cda9e933aaf409d17cc5d1be25ff73
CiSigEp <cisigep@gmail.com>
2019-09-11 13:12:17 -0500

    Updates

---
+++ precache-manifest.b26a11a87642dca69f437d2d26f9f596.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcbdeb04067d40b0a9f6a78cf78480c0",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "b738d9a9f1d2cc0714be",
+    "url": "/clicky-game/static/css/main.910aa034.chunk.css"
+  },
+  {
+    "revision": "2efcc525d9f6f37ecd8a",
+    "url": "/clicky-game/static/js/2.9372206a.chunk.js"
+  },
+  {
+    "revision": "b738d9a9f1d2cc0714be",
+    "url": "/clicky-game/static/js/main.79cfba3f.chunk.js"
+  },
+  {
+    "revision": "79ea0d5502c3a6ac0f74",
+    "url": "/clicky-game/static/js/runtime~main.80bf01e5.js"
+  }
+]);
